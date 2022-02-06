16cc1e08e2c25a507b79c7741c3774f7cf4819b0
Wei Sun <sunwei415@126.com>
2022-02-07 01:13:20 +0800

    done extracting sections and lectures

--- src/extracting_script.ts
+++ src/extracting_script.ts
@@ -1,12 +1,16 @@
 console.log("extracting Great")
 
+function extractCleanText(nameText: string) {
+  return nameText.replace(/\s+/g, " ").trim();
+}
+
 function extractLinks() {
   const x = document.querySelectorAll("a");
   const myArray:[string, string][] = [];
   for (let element of x) {
     const nameText = element.textContent;
     if (nameText) {
-      const cleanText = nameText.replace(/\s+/g, " ").trim();
+      const cleanText = extractCleanText(nameText);
       const cleanLink = element.href;
       myArray.push([cleanText, cleanLink]);
     }
@@ -16,12 +20,38 @@ function extractLinks() {
   return myArray
 }
 
-console.log(extractLinks()[0])
-
 // TODO: extract section titles
 
 function extractSectionTitles() {
+  let selectorForCourseSection = "div.course-section";
+  let selectorForSectionTitle = "div.section-title>span";
+  const sections = document.querySelectorAll(selectorForCourseSection);
 
+  for (let section of sections) {
+    const sectionTitleElement = section.querySelector(selectorForSectionTitle)
+    if (!sectionTitleElement) {
+      continue
+    }
+
+    console.log(extractCleanText(sectionTitleElement.textContent ?? ""))
+
+    const lectures = section.querySelectorAll<HTMLAnchorElement>(".section-item>a.item")
+    console.log(lectures.length)
+    for (let lecture of lectures) {
+
+      console.log(lecture.href)
+
+      const lectureNameElement = lecture.querySelector<HTMLSpanElement>("div.title-container>span.lecture-name")
+
+      if (lectureNameElement) {
+        console.log(extractCleanText(lectureNameElement.textContent ?? ""))
+      }
+
+
+    }
+  }
+
+  console.log(sections.length)
 }
 
 extractSectionTitles()
